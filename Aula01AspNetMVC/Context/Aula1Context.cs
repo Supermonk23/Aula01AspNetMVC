649e53b662e075485ac6a82b3076b52fcc123b6e
Supermonk23 <cavalheiro134@gmail.com>
2020-04-24 15:49:45 -0300

    Commit inicial
    Projeto relacionado as aulas 1, 2 e 3 de AspNet MVC Basico.

---
+++ Aula01AspNetMVC/Context/Aula1Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Aula01AspNetMVC.Models;
+
+namespace Aula01AspNetMVC.Context
+{
+    public class Aula1Context : DbContext
+    {
+        public Aula1Context()
+            : base("Aula1Context")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
